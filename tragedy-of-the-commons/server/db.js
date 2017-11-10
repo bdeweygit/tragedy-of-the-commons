540df820f5c6fafd4346684e260aee0c013d9a8d
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2017-11-09 19:46:16 -0500

    allow mongoose to connect to a database specified in a config.json file

--- tragedy-of-the-commons/server/db.js
+++ tragedy-of-the-commons/server/db.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
 
@@ -23,4 +25,23 @@ mongoose.model('Canvas', Canvas);
 //mongoose.model('Pixel', Pixel);
 
 // NOTE execute this file before using the Canvas model by calling require('db.js');
-mongoose.connect('mongodb://localhost/tragedy-of-the-commons');
+
+
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+
+  const fn = path.join(__dirname, 'config.json');
+  const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+  const conf = JSON.parse(data);
+  dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/tragedy-of-the-commons';
+}
+mongoose.connect(dbconf);
